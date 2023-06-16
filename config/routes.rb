8a3d37f21cbbafc8e9ec2bd871498bc4e383d228
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-16 12:22:58 +0200

    deploy
    Signed-off-by: benjamin cadars <benjamin.cadars@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: [:new, :index, :create, :show] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create, :index]
   end
 end
